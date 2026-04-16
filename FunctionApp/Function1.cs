aa8d5596686baacdaa8667965e7d5537a51aa1c6
Kristoffer Heby <kristofferheby@gmail.com>
2026-04-16 16:03:12 +0200

    Made the eightball

--- FunctionApp/Function1.cs
+++ FunctionApp/Function1.cs
@@ -17,8 +17,36 @@ namespace FunctionApp
         [Function("Function1")]
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
+
+            List<string> possibleAwnsers = [
+                "It is certain.",
+                "It is decidedly so.",
+                "Without a doubt.",
+                "Yes - definitely.",
+                "You may rely on it.",
+                "As I see it, yes.",
+                "Most likely.",
+                "Outlook good.",
+                "Yes.",
+                "Signs point to yes.",
+                "Reply hazy, try again.",
+                "Ask again later.",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again.",
+                "Don't count on it.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Very doubtful."
+                ];
+
+            Random rand = new Random();
+            int rint = rand.Next(0, possibleAwnsers.Count);
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+
+
+            return new OkObjectResult(possibleAwnsers[rint]);
         }
     }
 }
