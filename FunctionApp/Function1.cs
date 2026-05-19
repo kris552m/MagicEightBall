e12caa4001ec7649efd87934aa9afd088e1e8180
Kristoffer Heby <kristofferheby@gmail.com>
2026-05-19 23:12:50 +0200

    Andre linjer

--- FunctionApp/Function1.cs
+++ FunctionApp/Function1.cs
@@ -19,27 +19,38 @@ namespace FunctionApp
         {
 
             List<string> possibleAwnsers = [
-                "It is certain.",
-                "It is decidedly so.",
-                "Without a doubt.",
-                "Yes - definitely.",
-                "You may rely on it.",
-                "As I see it, yes.",
-                "Most likely.",
-                "Outlook good.",
-                "Yes.",
-                "Signs point to yes.",
-                "Reply hazy, try again.",
-                "Ask again later.",
-                "Better not tell you now.",
-                "Cannot predict now.",
-                "Concentrate and ask again.",
-                "Don't count on it.",
-                "My reply is no.",
-                "My sources say no.",
-                "Outlook not so good.",
-                "Very doubtful."
+                //"It is certain.",
+                //"It is decidedly so.",
+                //"Without a doubt.",
+                //"Yes - definitely.",
+                //"You may rely on it.",
+                //"As I see it, yes.",
+                //"Most likely.",
+                //"Outlook good.",
+                //"Yes.",
+                //"Signs point to yes.",
+                //"Reply hazy, try again.",
+                //"Ask again later.",
+                //"Better not tell you now.",
+                //"Cannot predict now.",
+                //"Concentrate and ask again.",
+                //"Don't count on it.",
+                //"My reply is no.",
+                //"My sources say no.",
+                //"Outlook not so good.",
+                //"Very doubtful."
+                "Line 1",
+                "Line 2",
+                "Line 3",
+                "Line 4",
+                "Line 5",
+                "Line 6",
+                "Line 7",
+                "Line 8",
+                "Line 9",
+                "Line 10"
                 ];
+
 
             Random rand = new Random();
             int rint = rand.Next(0, possibleAwnsers.Count);
